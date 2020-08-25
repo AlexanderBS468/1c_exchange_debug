46896b3e0aeee1199a1fac5b42f89c9c0e621d55
AlexBS <alex.sad.468@gmail.com>
2020-08-25 10:54:37 +0800

    #add add class ListImportFiles

--- bx_1c_import_lite.php
+++ bx_1c_import_lite.php
@@ -31,6 +31,74 @@ class ToolsDebug
 
 }
 
+class ListImportFiles {
+	private $arResulter = [];
+	private $option = [];
+
+	public function __construct($arg) {
+		$this->option = [
+			'path' => $arg["path"],
+			'absolute_path' => $arg["absolute_path"],
+		];
+
+		$dir = new Directory($this->option['path']);
+		if ($dir->isExists()) {
+			$arDir = $dir->getChildren();
+			self::getParentFiles($arDir);
+			foreach($arDir as $dirItem){
+				self::getFiles($dirItem);
+				if ($dirItem->isDirectory()) {
+					$this->arResulter['DIRECTORY'][] = $dirItem;
+					self::getChildDir($dirItem);
+				}
+			}
+		};
+	}
+
+	public function getParentFiles(&$arDir) {
+		foreach($arDir as $keyDirItem => &$dirItem){
+			if ($dirItem->isFile() && preg_match('/.*\.xml$/', $dirItem->getName())) {
+				if ($this->option['absolute_path']) {
+					$pattern = '/' . preg_replace('/\//', '\/', \Bitrix\Main\Application::getDocumentRoot() . '/upload/1c_catalog')  . '/';
+					$this->arResulter['FILES'][] = preg_replace($pattern, '', $dirItem->getPath());
+				} else {
+					$this->arResulter['FILES'][] = $dirItem->getPath();
+				}
+				unset($arDir[$keyDirItem]);
+			}
+		}
+	}
+
+	public function getListFiles() {
+		return $this->arResulter["FILES"];
+	}
+
+	protected function getFiles($dirItem) {
+		if ($dirItem->isFile() && preg_match('/.*\.xml$/', $dirItem->getName())) {
+			if ($this->option['absolute_path']) {
+				$pattern = '/' . preg_replace('/\//', '\/', \Bitrix\Main\Application::getDocumentRoot() . '/upload/1c_catalog')  . '/';
+				$this->arResulter['FILES'][] = preg_replace($pattern, '', $dirItem->getPath());
+			} else {
+				$this->arResulter['FILES'][] = $dirItem->getPath();
+			}
+		}
+	}
+
+	protected function getChildDir($dirItem) {
+		$dirChild = new Directory($dirItem->getPath());
+		if ($dirChild->isExists()) {
+			$arDir = $dirChild->getChildren();
+			foreach($arDir as $dirItem){
+				self::getFiles($dirItem);
+				if ($dirItem->isDirectory()) {
+					$this->arResulter['DIRECTORY'][] = $dirItem;
+					self::getChildDir($dirItem);
+				}
+			}
+		}
+	}
+}
+
 $_SESSION["BX_CML2_IMPORT"]["NS"]["STEP"]=0;
 ?>
 <html>
